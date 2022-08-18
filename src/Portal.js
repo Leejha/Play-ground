adc2aa6ed88785c34fef129e28e8ad2557e1a12f
이재하 <jhl2619@naver.com>
2022-08-18 20:15:22 +0900

    feat: Set up for study recoil

--- src/Portal.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-
-function Portal({ children }) {
-  const parent = document.getElementById('portal');
-  return ReactDOM.createPortal(children, parent);
-}
-
-export default Portal;
