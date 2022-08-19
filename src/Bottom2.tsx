fc671f7ea8548dbc29b33e09b63f4de3d26a00fa
이재하 <jhl2619@naver.com>
2022-08-19 18:41:09 +0900

    feat: Manage global state by using recoil

---
+++ src/Bottom2.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Bottom2() {
+  const bottom2Data = 'bottom data';
+  return <div>Bottom2</div>;
+}
+
+export default Bottom2;
---
+++ src/Middle.tsx
@@ -0,0 +1,14 @@
+// src/Middle.tsx
+
+import React from 'react';
+import Bottom from './Bottom';
+
+function Middle() {
+  return (
+    <>
+      <Bottom />
+    </>
+  );
+}
+
+export default Middle;
